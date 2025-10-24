3486788a26c676e81cd4afab9c236b60a2a30eea
Caique Barbosa <caiquebpa@gmail.com>
2025-10-24 14:43:37 -0300

    update

--- schemas/livro.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose")
-
-const livroSchema = new mongoose.Schema({
-    titulo: String,
-    autor: String,
-    valor: Number
-})
-
-module.exports = mongoose.model("livros", livroSchema)
